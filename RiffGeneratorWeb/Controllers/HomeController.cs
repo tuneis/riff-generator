caf2a7fddb963a6ba669cdbac365f06251b97cc2
Ryan Tunis <rtunis@niagaracollege.ca>
2018-03-02 15:43:58 -0500

    Removed unused code.

--- RiffGeneratorWeb/Controllers/HomeController.cs
+++ RiffGeneratorWeb/Controllers/HomeController.cs
@@ -24,22 +24,6 @@ namespace RiffGeneratorWeb.Controllers
 
         public IActionResult Index()
         {
-
-            //for (int i = 0; i < 10; i++)
-            //{
-            //    // get file location for saving and reading
-            //    string saveLocation = Path.Combine(_env.ContentRootPath + "/Files/", "GOGOGOGO" + i + ".mid");
-
-            //    // generate the midi sequence
-            //    MidiSequence midi = RiffGenerator.RiffGenerator.Randomize("Track Name", 1000, GeneralMidiInstrument.AcousticGrand);
-
-            //    // save to file
-            //    using (Stream file = System.IO.File.Create(saveLocation))
-            //    {
-            //        midi.Save(file);
-            //    }
-            //}
-
             var rgVM = new RGViewModel
             {
                 Instruments = new SelectList(RG.Instruments, "Item2", "Item1", RG.Instruments[(int)GeneralMidiInstrument.DistortionGuitar].ToString()),
@@ -48,41 +32,6 @@ namespace RiffGeneratorWeb.Controllers
                 Octaves = new SelectList(RG.Octaves, "Item2", "Item1"),
                 TimeSignatures = new SelectList(RG.TimeSignatures, "Item1", "Item1")
             };
-
-            //for (int i = 0; i < 10; i++)
-            //{
-            //    // get file location for saving and reading
-            //    string directory = Path.Combine(_env.WebRootPath, "midi");
-
-            //    // check if directory exists if not create it
-            //    if (!Directory.Exists(directory))
-            //        Directory.CreateDirectory(directory);
-
-            //    List<int> allowableOctaves = new List<int>() { 3 };
-            //    List<int> allowablePitches = new List<int>() { 8, 9 };
-            //    List<int> allowableDurations = new List<int>() { 2, 3 };
-
-            //    var rg = new RG("Track Name", GeneralMidiInstrument.DistortionGuitar, 200, new TimeSignature(4, 4), 400);
-
-            //    //var midi = rg.Randomize();
-            //    //// save to file
-            //    //using (Stream file = System.IO.File.Create(Path.Combine(directory, $"test{i}.mid")))
-            //    //{
-            //    //    midi.Save(file);
-            //    //}
-            //    // generate the riff as a midi sequence
-            //    var midi = rg.Generate(
-            //        allowableOctaves,
-            //        allowablePitches,
-            //        allowableDurations
-            //    );
-
-            //    // save to file
-            //    using (Stream file = System.IO.File.Create(Path.Combine(directory, $"test{i}.mid")))
-            //    {
-            //        midi.Save(file);
-            //    }
-            //}
             return View(rgVM);
         }
 
@@ -102,12 +51,14 @@ namespace RiffGeneratorWeb.Controllers
             if (!Directory.Exists(directory))
                 Directory.CreateDirectory(directory);
 
+            // instantiate the riff generator
             var rg = new RG(vm.TrackName, RG.Instruments[vm.SelectedInstrument].Item3, vm.Tempo, RG.TimeSignatures[vm.SelectedTimeSignature].Item3, vm.TotalNotes);
 
+            // generate the midi based on selected values
             var midi = rg.Generate(vm.SelectedOctaves, vm.SelectedPitches, vm.SelectedDurations);
 
+            // get date for file parsing
             var now = DateTime.Now;
-
             var filePath = Path.Combine(directory, $"{vm.TrackName.Replace(" ", string.Empty)}_{Guid.NewGuid().ToString()}.mid");
 
             // save to file
@@ -116,6 +67,7 @@ namespace RiffGeneratorWeb.Controllers
                 midi.Save(file);
             }
 
+            // generate memory stream for download
             var memory = new MemoryStream();
             using (var stream = new FileStream(filePath, FileMode.Open))
             {
